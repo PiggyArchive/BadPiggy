5c3aecde64a69846e4294693054db55a16bf3765
Mr. Piggy <mcpepig123@gmail.com>
2016-07-12 17:35:14 -0700

    Fixed formatting with PHPDesigner >:D

--- src/BadPiggy/BadPiggyTick.php
+++ src/BadPiggy/BadPiggyTick.php
@@ -6,51 +6,51 @@ use pocketmine\network\protocol\SetTimePacket;
 use pocketmine\scheduler\PluginTask;
 
 class BadPiggyTick extends PluginTask{
-	public function __construct($plugin){
-		parent::__construct($plugin);
-		$this->plugin = $plugin;
-	}
-	
-	public function onRun($currentTick){
-		foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
-			if(isset($this->plugin->spam[strtolower($player->getName())])){
-				$messages = array(
-					"§aCLICK HERE FOR FREE BACON",
-					"§aCHECK OUT MCPEPIG's PLUGINS!",
-					"§aTROLOLOLOLLOL",
-					"§aSPAM! SPAM! SPAM!",
-					"§aCHECK OUT THE PUNISHMENTAL PLUGIN FOR MCPC",
-					"§aHI BYE HI BYE HI BYE HI BYE"
-				);
-				$message = array_rand($messages, 1);
-				$message = $messages[$message];
-				$player->sendMessage($message);
-			}
-			if(isset($this->plugin->rewind[strtolower($player->getName())])){
-				$pk = new SetTimePacket();
-				$pk->time = 0;
-				if(isset($this->plugin->unrewind[strtolower($player->getName())])){
-					if($this->plugin->unrewind[strtolower($player->getName())] == 23549){
-						$this->plugin->rewind[strtolower($player->getName())] = $this->plugin->rewind[strtolower($player->getName())] - 50;
-						$pk->time = $this->plugin->rewind[strtolower($player->getName())];
-						if($this->plugin->rewind[strtolower($player->getName())] < 0){
-							$pk->time = 0;
-							$this->plugin->unrewind[strtolower($player->getName())] = 0;
-							$this->plugin->rewind[strtolower($player->getName())] = 23549;
-						}
-					}else{
-						$this->plugin->unrewind[strtolower($player->getName())] = $this->plugin->unrewind[strtolower($player->getName())] + 50;
-						$pk->time = $this->plugin->unrewind[strtolower($player->getName())];
-						if($this->plugin->unrewind[strtolower($player->getName())] < 0){
-							$pk->time = 23549;
-							$this->plugin->unrewind[strtolower($player->getName())] = 23549;
-						}						
-					}
-				}
-				$pk->started = $player->getLevel()->stopTime == false;
-				$player->dataPacket($pk);
-			}
-		}
-	}
-	
+    public function __construct($plugin){
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+    }
+
+    public function onRun($currentTick){
+        foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+            if(isset($this->plugin->spam[strtolower($player->getName())])){
+                $messages = array(
+                    "§aCLICK HERE FOR FREE BACON",
+                    "§aCHECK OUT MCPEPIG's PLUGINS!",
+                    "§aTROLOLOLOLLOL",
+                    "§aSPAM! SPAM! SPAM!",
+                    "§aCHECK OUT THE PUNISHMENTAL PLUGIN FOR MCPC",
+                    "§aHI BYE HI BYE HI BYE HI BYE"
+                );
+                $message = array_rand($messages, 1);
+                $message = $messages[$message];
+                $player->sendMessage($message);
+            }
+            if(isset($this->plugin->rewind[strtolower($player->getName())])){
+                $pk = new SetTimePacket();
+                $pk->time = 0;
+                if(isset($this->plugin->unrewind[strtolower($player->getName())])){
+                    if($this->plugin->unrewind[strtolower($player->getName())] == 23549) {
+                        $this->plugin->rewind[strtolower($player->getName())] = $this->plugin->rewind[strtolower($player->getName())] - 50;
+                        $pk->time = $this->plugin->rewind[strtolower($player->getName())];
+                        if($this->plugin->rewind[strtolower($player->getName())] < 0){
+                            $pk->time = 0;
+                            $this->plugin->unrewind[strtolower($player->getName())] = 0;
+                            $this->plugin->rewind[strtolower($player->getName())] = 23549;
+                        }
+                    }else{
+                        $this->plugin->unrewind[strtolower($player->getName())] = $this->plugin->unrewind[strtolower($player->getName())] + 50;
+                        $pk->time = $this->plugin->unrewind[strtolower($player->getName())];
+                        if($this->plugin->unrewind[strtolower($player->getName())] < 0){
+                            $pk->time = 23549;
+                            $this->plugin->unrewind[strtolower($player->getName())] = 23549;
+                        }
+                    }
+                }
+                $pk->started = $player->getLevel()->stopTime == false;
+                $player->dataPacket($pk);
+            }
+        }
+    }
+
 }
--- src/BadPiggy/Sounds/TNTPrimeSound.php
+++ src/BadPiggy/Sounds/TNTPrimeSound.php
@@ -6,8 +6,8 @@ use pocketmine\level\sound\GenericSound;
 use pocketmine\math\Vector3;
 
 class TNTPrimeSound extends GenericSound{
-	public function __construct(Vector3 $pos, $pitch = 0){
-		parent::__construct($pos, 1005, $pitch);
-	}
-	
+    public function __construct(Vector3 $pos, $pitch = 0){
+        parent::__construct($pos, 1005, $pitch);
+    }
+
 }
